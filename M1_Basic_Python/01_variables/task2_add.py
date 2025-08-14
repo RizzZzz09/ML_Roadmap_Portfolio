9de5aea59ffc5c56003288c416a29def22920a10
RizzZzz09 <136895934+RizzZzz09@users.noreply.github.com>
2025-08-14 18:25:15 +0300

    M1.1 — Variables: Complete all tasks (01–03)

---
+++ M1_Basic_Python/01_variables/task2_add.py
@@ -0,0 +1,9 @@
+def main():
+    first_number = float(input("Enter the first number: "))
+    second_number = float(input("Enter the second number: "))
+
+    print(f"Result: {first_number + second_number}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ M1_Basic_Python/01_variables/task3_check.py
@@ -0,0 +1,8 @@
+def main():
+    user_input = float(input("Enter any number: "))
+
+    print(user_input > 10)
+
+
+if __name__ == "__main__":
+    main()
