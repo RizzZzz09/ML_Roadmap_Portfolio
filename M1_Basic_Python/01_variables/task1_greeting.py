d0ccdfd20139208723309030ed2926f0aa38ccb5
RizzZzz09 <136895934+RizzZzz09@users.noreply.github.com>
2025-08-14 18:25:15 +0300

    M1.1 - Complete variables practice (tasks 1-3)

---
+++ M1_Basic_Python/01_variables/task1_greeting.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("What is your name: ")
+    age = int(input("How old are you: "))
+
+    print(f"Hello, {name}! You are {age} years old.")
+
+
+if __name__ == "__main__":
+    main()
