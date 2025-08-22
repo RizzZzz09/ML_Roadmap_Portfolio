cf9b56f3bd20e5f520bfc60746bd0aaf334e48ec
RizzZzz09 <136895934+RizzZzz09@users.noreply.github.com>
2025-08-22 15:37:32 +0300

    M1.7 — Modules & Packages (step 3): __name__/__main__ guard, import without side effects

---
+++ M01_Basic_Python/07_modules_packages/step3_main_guard/app/main.py
@@ -0,0 +1,26 @@
+from .core.operations import add, add_list, div, mul, sub
+from .utils.printer import printer
+
+
+def main():
+    result = add(2, 2)
+    printer(result)
+
+    result = sub(10, 5)
+    printer(result)
+
+    result = mul(4, 4)
+    printer(result)
+
+    result = div(10, 2)
+    printer(result)
+
+    result = div(10, 0)
+    printer(result)
+
+    result = add_list([1, 2, 3, 4, 5], [1, 2])
+    printer(result)
+
+
+if __name__ == "__main__":
+    main()
